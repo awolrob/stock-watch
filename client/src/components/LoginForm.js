61243404f8ba14f5c48f3de15049a5f0d7a64a5f
Fausnightm <m.fausnight@lafilm.edu>
2021-10-23 09:17:50 -0400

    Working on implementing ANTD

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,3 +1,4 @@
+
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
